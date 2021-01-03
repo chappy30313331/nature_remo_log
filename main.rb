60aa0a18ef7b6b450ea6986366dea63cf01ef582
chappy30313331 <46666464+chappy30313331@users.noreply.github.com>
2021-01-03 14:34:29 +0900

    dbの設定をdatabase.ymlに切り出し

--- main.rb
+++ main.rb
@@ -1,17 +1,13 @@
 require 'uri'
 require 'json'
 require 'http'
+require 'erb'
 require 'active_record'
 require 'dotenv/load'
 require './slack_client'
 
-ActiveRecord::Base.establish_connection(
-  adapter: 'mysql2',
-  database: ENV['DB_DATABASE'],
-  host: ENV['DB_HOST'],
-  username: ENV['DB_USERNAME'],
-  password: ENV['DB_PASSWORD']
-)
+config = YAML::load(ERB.new(IO.read('./database.yml')).result)
+ActiveRecord::Base.establish_connection(config.fetch(ENV.fetch('RUBY_ENV') { 'production' }))
 Time.zone_default = Time.find_zone! 'Tokyo'
 ActiveRecord::Base.default_timezone = :local
 
